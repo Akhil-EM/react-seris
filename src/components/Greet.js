7ee6b8f0ccd6d71da5706ad890c3fca3c614a243
Akhil-EM <akhilemcalicut@gmail.com>
2021-06-20 06:56:56 +0530

    class componets maintains a state. it must use react and component

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -13,3 +13,11 @@ export default Greet;///by default we can import it as in any name
 // export const Greet=()=>{
 //      return(<h1>Hello user . here iam </h1>)
 // } this let to use only in the name Greet
+
+
+// simple functions
+// use func as much as possible
+// absence of this key word
+// solution without using if
+// mainly responsible for the ui
+// Stateless/Dumb/Presentational
---
+++ src/components/Welcome.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Welcome extends React.Component{
+
+    render(){
+        return(
+            <h1>Welcome here.....</h1>
+        );
+    }
+}
+
+export default Welcome;
+
+// More feature rich ui
+// Maintain their own private data-sate
+// Complex ui logic
+// Provide Life cycle hooks
+// Stateful/Smart/Container
+
+
