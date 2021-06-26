f3a7d5e625350fde6dd16a166fe519e6b97e4e28
Akhil-EM <akhilemcalicut@gmail.com>
2021-06-26 12:41:54 +0530

    fragment do not show in the browser, use React.Fragment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
+import Table from './components/Table';
+
 // import Hello from './components/Hello'
 // import Welcome from './components/Welcome'
 // import EventClass from './components/EventClass'
@@ -9,16 +11,15 @@ import './App.css';
 // import ListComponent from './components/ListComponent'
 // import StyleSheet from './components/StyleSheet'
 // import Form from './components/Form';
-import LifeCycleA from './components/LifeCycleA';
-
-
+// import LifeCycleA from './components/LifeCycleA';
+// import FragmentDemo from './components/FragmentDemo';
 
  
 function App() {
   
   return (
     <div className="App">
-         <LifeCycleA/>
+         <Table/>
     </div>
   );
 }
---
+++ src/components/FragmentDemo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class FragmentDemo extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>
+                This is demo of fragment.
+                </h1>
+                <p>iam the just a same paragraph</p>
+            </React.Fragment>
+        )
+    }
+}
